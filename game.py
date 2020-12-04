2457222aae6e0fdccb155d169f253fa3011704fa
Catalin Crainiceanu <c.crainiceanu@gmail.com>
2020-12-04 21:17:10 +0200

    Added function to get categories from file

--- game.py
+++ game.py
@@ -1,5 +1,14 @@
 import random as r
 
+def get_categories(filename):
+    categories = []
+    file = open(filename, 'r')
+    for i in file.readlines():
+        categories.append(i.rstrip())
+    file.close()
+    return categories
+
+
 def get_info(filename):
     words = []
     file = open(filename, 'r')
@@ -8,15 +17,21 @@ def get_info(filename):
     print (words)
     nr = int(r.uniform(0, len(words)))
     print (words[nr])
-
+    file.close()
 
 
 if __name__ == "__main__":
-    print("Categorii disponibile: \n 1. Nume \n 2. Sport \n")
-    user_category = int(input("Alege o categorie: "))
-    if (user_category == 1):
-        filename = 'nume'
-    elif (user_category == 2):
-        filename = 'sport'
+    ### Categories ###
+    categories = get_categories("category")
+    print("\nCategorii disponibile: ")
+    for i in range(len(categories)):
+        print('     {}. {}'.format(i + 1, categories[i]))
 
+    ### User Input Category
+    user_category = int(input("\nAlege o categorie: "))
+    for i in range(len(categories)):
+        if user_category == i + 1:
+            filename = categories[i]
+
+    ### Get words from choosen category
     get_info(filename)
